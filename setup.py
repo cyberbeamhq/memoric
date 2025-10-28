d9a3610d093aab93ccf4cfff1f80d5c4db017f68
Muthanna Alfaris <114227787+M-Alfaris@users.noreply.github.com>
2025-10-28 18:26:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,23 +29,36 @@ setup(
     long_description_content_type="text/markdown",
     author="Muthanna Alfaris",
     url="https://github.com/cyberbeamhq/memoric",
-    license="MIT",
+    license="Apache-2.0",
     packages=find_packages(exclude=("tests", "examples")),
     include_package_data=True,
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=install_requires,
+    extras_require={
+        "dev": ["pytest", "pytest-cov", "black", "flake8", "mypy", "build"],
+    },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
+        
     ],
     project_urls={
         "Source": "https://github.com/cyberbeamhq/memoric",
         "Tracker": "https://github.com/cyberbeamhq/memoric/issues",
     },
+    entry_points={
+        "console_scripts": [
+            "memoric=memoric_cli:main",
+        ]
+    },    
 )
 
 
